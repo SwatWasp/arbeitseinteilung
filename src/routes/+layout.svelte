8693b904359c3f0b410c9c6f84fba10795b20423
SwatWasp <ramon.scherrer95@gmail.com>
2025-01-03 15:33:16 +0100

    Layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    let { children } = $props();
+    import "./layout.css";
+</script>
+
+<ul class="nav">
+    <li>
+        <img src="https://stvdo.ch/wp-content/uploads/2017/11/Logo.svg" alt="Logo">
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="/">Home</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/personen">Personen</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/einsaetze">Einsätze</a>
+    </li>
+</ul>
+
+<div class="container mt-4">
+    {@render children()}
+</div>
